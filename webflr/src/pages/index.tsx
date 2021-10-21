dc728165abd1ee900f3ccf136c7d9949d0685d97
Guilherme Jordão <lgjmoraes@hotmail.com>
2021-10-21 13:11:44 -0300

    Add Sass

--- webflr/src/pages/index.tsx
+++ webflr/src/pages/index.tsx
@@ -1,9 +1,14 @@
+import Head from "next/head";
 
-
-
-
+("");
 export default function Home() {
   return (
-    <h1>Site FLR</h1>
-  )
+    <>
+      {/* configura o title em cada pagina */}
+      <Head>
+        <title>Pagina Inicial FLR</title>
+      </Head>
+      <h1>Site FLR</h1>
+    </>
+  );
 }
