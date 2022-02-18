57d5ed11c097c2dd93cbb064db9426ebf87f839a
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2022-02-18 19:38:55 -0300

    Update index.tsx

--- webflr/src/components/Header/index.tsx
+++ webflr/src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import { ActiveLink } from '../ActiveLink';
 
 
-export function Header() {
+export function Header() { 
 
 
   return (
@@ -14,17 +14,11 @@ export function Header() {
               <img src="/images/logo_flr.svg" alt="logo FLR"></img>               
         <nav>                    
             <ActiveLink activeClassName={styles.active} href="/Contabilidade"><a>Contabilidade</a></ActiveLink>
-
-            <ActiveLink activeClassName={styles.active}  href="/Pericia"><a>Pericia</a></ActiveLink>
-
+            <ActiveLink activeClassName={styles.active}  href="/Pericia"><a>Pericia a</a></ActiveLink>
             <ActiveLink activeClassName={styles.active}href="/Obras"><a>Regularização Obras</a></ActiveLink>
-
             <ActiveLink activeClassName={styles.active}href="/Curso"><a >Curso / Mentoria</a></ActiveLink>
-
             <ActiveLink activeClassName={styles.active}href="/Downloads"><a>Downloads</a></ActiveLink>
-
             <ActiveLink activeClassName={styles.active}href="/Contato"><a>Contato</a></ActiveLink>
-
             <ActiveLink activeClassName={styles.active}href="/QuemSou"><a>Quem Sou</a></ActiveLink>
         </nav>
       </div>
