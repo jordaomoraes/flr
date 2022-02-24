e279e0b20fdde5525f6de98136664971225f36d8
Guilherme Jordão <62813340+jordaomoraes@users.noreply.github.com>
2022-02-24 15:56:20 -0300

    feat - teste

--- webflr/src/pages/Pericia/index.tsx
+++ webflr/src/pages/Pericia/index.tsx
@@ -4,7 +4,7 @@ import styles from "../Contato/";
 
 // Página que contém os componentes do café crú
 const Pericia: React.FC = () => {
-  return <h1>Pagina de Pericia</h1>;
+  return <h1>Atualização da pagina de Pericia</h1>;
 };
 
 export default Pericia;
